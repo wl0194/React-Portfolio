d7909841ba03faa6c624c773ff04e2e677446616
Amy Hwang <728amyh@gmail.com>
2022-02-07 14:22:28 -0500

    adding profile img

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -1,11 +1,12 @@
 import "./aboutme.scss"
+import profile_image from './profile_image'
 
 export default function AboutMe() {
     return (
         <div className="AboutMe" id="aboutMe">
             <div className="left">
                 <div className="imgContainer">
-                    <img src="./assets/profile.png" alt=""/>
+                <img src={profile_image} />
                 </div>
             </div>
             <div className="right"></div>
