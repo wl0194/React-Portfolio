da5e1dd1c832ce489a4832938651994329ec4bac
Amy Hwang <728amyh@gmail.com>
2022-02-07 13:50:08 -0500

    adding profile img to html

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -3,7 +3,12 @@ import "./aboutme.scss"
 export default function AboutMe() {
     return (
         <div className="AboutMe" id="aboutMe">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="./assets/profile.png" alt=""/>
+                </div>
+            </div>
+            <div className="right"></div>
         </div>
     )
 }
--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -3,8 +3,10 @@ import "./intro.scss"
 export default function intro() {
     return (
         <div className="intro" id="intro">
-            
-            
+          <h1>HELLO;</h1>
+
+
+
         </div>
     )
 }
