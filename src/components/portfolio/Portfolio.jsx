6aaaefe7e84952d641de318b0decfdd53614f1d3
Amy Hwang <728amyh@gmail.com>
2022-02-09 18:25:35 -0500

    working on adding projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,42 @@
 import "./portfolio.scss"
 
+
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            
-        </div>
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">
+                    Featured Projects
+                </li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Cyclist</h3>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Cyclist</h3>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Cyclist</h3>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Cyclist</h3>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Cyclist</h3>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Cyclist</h3>
+                </div>
+                
+            </div>
+          </div>  
     )
 }
