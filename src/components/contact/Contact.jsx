67c2fcaffe483d01fcc8013a3c25768acfd8c73d
Amy Hwang <728amyh@gmail.com>
2022-02-10 23:42:55 -0500

    working on contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,15 +1,22 @@
 import "./contact.scss"
 
+
 export default function Contact() {
     return (
         <div className="contact" id="contact">
             <div className="left">
-                <img src="" alt=""/>
+                <h1>Let's Connect!</h1>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <p>Like what you're seeing? would like to work as a team?</p>
             </div>
             <div className="right">
-                <h2>Contact</h2>
+                <h2>Hire Me</h2>
                 <form>
-                    <input type="text" placeholder="Email"/>
+                    <input type="text" placeholder="Email" />
                     <textarea placeholder="Message"></textarea>
                     <button>Send</button>
                 </form>
